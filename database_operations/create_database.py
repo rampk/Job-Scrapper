6178668d6f60fa1ac2d9168f1f1a887d68648a22
Ramprakash <prakashram1996@gmail.com>
2020-12-03 22:34:42 +0530

    Loading the data to DB V1.0

--- database_operations/create_database.py
+++ database_operations/create_database.py
@@ -1,7 +1,8 @@
 import sqlite3
+from datetime import datetime
 
 # Creating/Connecting the db
-db_connection = sqlite3.connect('Data/Database/job_database.db')
+db_connection = sqlite3.connect('../Data/Database/job_database.db')
 
 # Opening a cursor
 db_cursor = db_connection.cursor()
@@ -15,7 +16,7 @@ db_cursor.execute("""CREATE TABLE job_details(
                     job_description text,
                     job_posted text,
                     posted_website text,
-                    job_link text,
+                    job_link text NOT NULL PRIMARY KEY,
                     transaction_id int,
                     created_date_time text,
                     modified_date_time text
@@ -23,11 +24,16 @@ db_cursor.execute("""CREATE TABLE job_details(
 
 # Create Table jobs to store transaction details
 db_cursor.execute("""CREATE TABLE transaction_details(
-                    transaction_id int,
+                    transaction_id int NOT NULL PRIMARY KEY,
                     user_id text,
                     created_date_time text,
                     modified_date_time text
     )""")
+
+# Create a zeroth record for transaction, so future one starts with 1
+current_time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+insert_record = (0, 'admin', current_time, current_time)
+db_cursor.execute('insert into transaction_details values (?,?,?,?)', insert_record)
 
 # Commit the changes
 db_connection.commit()
