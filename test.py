19ddc8de2bd125ad5f9fcd96f1077e61a7ea153a
Ramprakash Pavithrakannan <67750688+rampk@users.noreply.github.com>
2020-12-04 00:17:42 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-db_connection = sqlite3.connect('../Data/Database/job_database.db')
-
-db_cursor = db_connection.cursor()
-db_cursor.execute('select * from job_details')
-print(db_cursor.fetchall())
